a7a811fd750dea0a9fa89a47ac86ab7ac38f8cdc
Jack Wearden <jack@jackwearden.co.uk>
2023-08-27 00:40:08 +0100

    quiet apt-get install

--- build.bash
+++ build.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e -o pipefail
 
-apt-get -qq update
-apt-get -qq install -y libsystemd-dev libseccomp-dev golang-github-proglottis-gpgme-dev make golang-go git python3 bash
+apt-get update -qq
+apt-get install -qq -y libsystemd-dev libseccomp-dev golang-github-proglottis-gpgme-dev make golang-go git python3 bash
 git config --global --add safe.directory '*'
 cd /podman
 make binaries docs completions install install.completions DESTDIR=./deb PREFIX=/usr ETCDIR=/etc SHELL=/bin/bash
